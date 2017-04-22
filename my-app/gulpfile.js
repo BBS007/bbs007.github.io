91d5654ee09a0c0f9146a870755f5c2f3e693c9e
BBS007 <b.barbesange@outlook.com>
2017-04-23 00:42:35 +0200

    Added a clean task in gulp that will be done before deploy.

--- my-app/gulpfile.js
+++ my-app/gulpfile.js
@@ -1,6 +1,21 @@
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('deploy', () => {
-    gulp.src('dist/*')
+    return gulp.src('dist/*', {read: false})
         .pipe(gulp.dest('../'));
+});
+
+
+gulp.task('clean', () => {
+    return del([
+        // Delete everything
+        '../*',
+
+        // But hidden files
+        '!../.*',
+
+        // And the my-app directory
+        '!../my-app'
+    ]);
 });
